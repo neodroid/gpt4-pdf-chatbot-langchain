bca98881205fd66ce5c8cc47d182b0996cc2570d
Kevin Ahmad <kevinahmad.f@gmail.com>
2023-05-01 02:41:03 +0700

    remove fs file

--- utils/customPDFLoader.ts
+++ utils/customPDFLoader.ts
@@ -1,35 +1,18 @@
 import { Document } from 'langchain/document';
-import { readFile } from 'fs/promises';
 import { BaseDocumentLoader } from 'langchain/document_loaders';
 
-export abstract class BufferLoader extends BaseDocumentLoader {
-  constructor(public filePathOrBlob: string | Blob) {
+export class CustomPDFLoader extends BaseDocumentLoader {
+  constructor(private blob: Blob) {
     super();
   }
 
-  protected abstract parse(
-    raw: Buffer,
-    metadata: Document['metadata'],
-  ): Promise<Document[]>;
-
   public async load(): Promise<Document[]> {
-    let buffer: Buffer;
-    let metadata: Record<string, string>;
-    if (typeof this.filePathOrBlob === 'string') {
-      buffer = await readFile(this.filePathOrBlob);
-      metadata = { source: this.filePathOrBlob };
-    } else {
-      buffer = await this.filePathOrBlob
-        .arrayBuffer()
-        .then((ab) => Buffer.from(ab));
-      metadata = { source: 'blob', blobType: this.filePathOrBlob.type };
-    }
+    const buffer = await this.blob.arrayBuffer().then((ab) => Buffer.from(ab));
+    const metadata = { source: 'blob', blobType: this.blob.type };
     return this.parse(buffer, metadata);
   }
-}
 
-export class CustomPDFLoader extends BufferLoader {
-  public async parse(
+  protected async parse(
     raw: Buffer,
     metadata: Document['metadata'],
   ): Promise<Document[]> {
